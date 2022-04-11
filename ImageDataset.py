b1d99bc1f24dc63a38f828b38a0ed6b43bcee620
RahimD <46304915+RahimD@users.noreply.github.com>
2022-04-11 11:10:33 -0400

    Train a single channel's model at a time
    I used to train all 3 at once, but memory considerations force me to be realistic. Trying one at a time, and changed the ImageDataset to load given channel based data at a time.

--- ImageDataset.py
+++ ImageDataset.py
@@ -4,7 +4,7 @@ import torch.utils.data
 import torchvision.transforms.functional
 
 class ImageDataset(torch.utils.data.Dataset):
-    def __init__(self, train_image_names, train_dir, target_dir, crop_h, crop_w, factor):
+    def __init__(self, train_image_names, train_dir, target_dir, crop_h, crop_w, channel, factor):
         self._train_names = train_image_names
         self._target_names = []
         self._train_dir = train_dir
@@ -12,6 +12,7 @@ class ImageDataset(torch.utils.data.Dataset):
         self._cropped_h = crop_h
         self._cropped_w = crop_w
         self._factor = factor
+        self._channel = channel
         for name in self._train_names:
             self._target_names.append(name.split("x")[0]+".png")
 
@@ -30,21 +31,21 @@ class ImageDataset(torch.utils.data.Dataset):
         train_size = (train_img.size[1], train_img.size[0])
         tar_size = (tar_img.size[1], tar_img.size[0])
        
-        train_r_tsr = torch.tensor(list(train_img.getdata(0)), dtype=torch.float32).view(1, train_size[0],train_size[1])
-        tar_r_tsr = torch.tensor(list(tar_img.getdata(0)), dtype=torch.float32).view(1, tar_size[0],tar_size[1])
+        train_r_tsr = torch.tensor(list(train_img.getdata(self._channel)), dtype=torch.float16).view(1, train_size[0],train_size[1])
+        tar_r_tsr = torch.tensor(list(tar_img.getdata(self._channel)), dtype=torch.float16).view(1, tar_size[0],tar_size[1])
 
-        train_g_tsr = torch.tensor(list(train_img.getdata(1)), dtype=torch.float32).view(1, train_size[0],train_size[1])
-        tar_g_tsr = torch.tensor(list(tar_img.getdata(1)), dtype=torch.float32).view(1, tar_size[0],tar_size[1])
+        # train_g_tsr = torch.tensor(list(train_img.getdata(1)), dtype=torch.float32).view(1, train_size[0],train_size[1])
+        # tar_g_tsr = torch.tensor(list(tar_img.getdata(1)), dtype=torch.float32).view(1, tar_size[0],tar_size[1])
 
-        train_b_tsr = torch.tensor(list(train_img.getdata(2)), dtype=torch.float32).view(1, train_size[0],train_size[1])
-        tar_b_tsr = torch.tensor(list(tar_img.getdata(2)), dtype=torch.float32).view(1, tar_size[0],tar_size[1])
+        # train_b_tsr = torch.tensor(list(train_img.getdata(2)), dtype=torch.float32).view(1, train_size[0],train_size[1])
+        # tar_b_tsr = torch.tensor(list(tar_img.getdata(2)), dtype=torch.float32).view(1, tar_size[0],tar_size[1])
 
         train_img.close()
         tar_img.close()
 
         train_r_tsr, tar_r_tsr = self.crop_image(train_r_tsr,tar_r_tsr)
-        train_g_tsr, tar_g_tsr = self.crop_image(train_g_tsr,tar_g_tsr)
-        train_b_tsr, tar_b_tsr = self.crop_image(train_b_tsr,tar_b_tsr)
+        # train_g_tsr, tar_g_tsr = self.crop_image(train_g_tsr,tar_g_tsr)
+        # train_b_tsr, tar_b_tsr = self.crop_image(train_b_tsr,tar_b_tsr)
 
         #When doing the convolutions, you lose 1 pixel on both H and W, so when you upscale via PixelShuffle, you lose 4 on each spatial dimension
         #Thus, I cut 4 rows and 4 columns from target image. I suppose I could've used "crop()" instead
@@ -54,14 +55,20 @@ class ImageDataset(torch.utils.data.Dataset):
         tar_r_tsr = torch.index_select(tar_r_tsr,1,desiredRows)
         tar_r_tsr = torch.index_select(tar_r_tsr,2,desiredCols)
         
-        tar_g_tsr = torch.index_select(tar_g_tsr,1,desiredRows)
-        tar_g_tsr = torch.index_select(tar_g_tsr,2,desiredCols)
+        # tar_g_tsr = torch.index_select(tar_g_tsr,1,desiredRows)
+        # tar_g_tsr = torch.index_select(tar_g_tsr,2,desiredCols)
 
-        tar_b_tsr = torch.index_select(tar_b_tsr,1,desiredRows)
-        tar_b_tsr = torch.index_select(tar_b_tsr,2,desiredCols)
+        # tar_b_tsr = torch.index_select(tar_b_tsr,1,desiredRows)
+        # tar_b_tsr = torch.index_select(tar_b_tsr,2,desiredCols)
 
         #print("Loaded: " + self._train_names[idx] ,self._target_names[idx] + " Sizes: " + str(train_r_tsr.shape), str(tar_r_tsr.shape))
 
-        return self._train_names[idx], train_r_tsr, tar_r_tsr, train_g_tsr, tar_g_tsr, train_b_tsr, tar_b_tsr
+        return                  \
+        train_r_tsr.to('cuda'), \
+        tar_r_tsr.to('cuda')#,   \
+        # train_g_tsr.to('cuda'), \
+        # tar_g_tsr.to('cuda'),   \
+        # train_b_tsr.to('cuda'), \
+        # tar_b_tsr.to('cuda')
         
         
--- ImageSR.py
+++ ImageSR.py
@@ -21,78 +21,78 @@ BATCH_SIZE = 1
 BATCHES = int(len(train_dataset) / BATCH_SIZE)
 EPOCHS = int(TOTAL_ITERATIONS / len(train_dataset))
 
-d_set = ImageDataset.ImageDataset(train_dataset, train_dir, target_dir, lr_h, lr_w, FACTOR)
-d_loaded = torch.utils.data.DataLoader(d_set, batch_size = BATCH_SIZE, shuffle = True)
-
-v_set = ImageDataset.ImageDataset(train_dataset, train_dir, target_dir, lr_h, lr_w, FACTOR)
-v_loaded = torch.utils.data.DataLoader(v_set, batch_size = 2*BATCH_SIZE)
-
-model_r = ImageSRModel.ImageSRModel(FACTOR)
-model_b = ImageSRModel.ImageSRModel(FACTOR)
-model_g = ImageSRModel.ImageSRModel(FACTOR)
+model_r = ImageSRModel.ImageSRModel(FACTOR).to('cuda')
+# model_b = ImageSRModel.ImageSRModel(FACTOR).to('cuda')
+# model_g = ImageSRModel.ImageSRModel(FACTOR).to('cuda')
 optimizer_r = torch.optim.Adam(model_r.parameters())
-optimizer_g = torch.optim.Adam(model_g.parameters())
-optimizer_b = torch.optim.Adam(model_b.parameters())
-for epoch in range(EPOCHS):
-    batch_num = 0
-    batch_losses = [[],[],[]]
-    model_r.train()
-    model_g.train()
-    model_b.train()
-    for img_name, lr_r_img, hr_r_img, lr_g_img, hr_g_img, lr_b_img, hr_b_img in d_loaded:
-            print("EPOCH: " + str(epoch) + "BATCH: " + str(batch_num))
-            pred_r = model_r(lr_r_img)
-            pred_g = model_g(lr_g_img)
-            pred_b = model_b(lr_b_img)
-            
-            loss_r = torch.nn.functional.mse_loss(pred_r,hr_r_img, reduction="sum")
-            loss_g = torch.nn.functional.mse_loss(pred_g,hr_g_img, reduction="sum")
-            loss_b = torch.nn.functional.mse_loss(pred_b,hr_b_img, reduction="sum")
-            
-            print(float(loss_r))
-            print(float(loss_g))
-            print(float(loss_b))
+# optimizer_g = torch.optim.Adam(model_g.parameters())
+# optimizer_b = torch.optim.Adam(model_b.parameters())
+c = ["r", "g", "b"]
+for channel in range(3):
+    d_set = ImageDataset.ImageDataset(train_dataset, train_dir, target_dir, lr_h, lr_w, channel, FACTOR)
+    d_loaded = torch.utils.data.DataLoader(d_set, batch_size = BATCH_SIZE, shuffle = True)
 
-            batch_losses[0].append(float(loss_r))
-            batch_losses[1].append(float(loss_g))
-            batch_losses[2].append(float(loss_b))
-            
-            loss_r.backward()
-            optimizer_r.step()
-            optimizer_r.zero_grad()
-            loss_g.backward()
-            optimizer_g.step()
-            optimizer_g.zero_grad()
-            loss_b.backward()
-            optimizer_b.step()
-            optimizer_b.zero_grad()
-            batch_num+=1
-    
-    #Validation Loop After Each Epoch
-    model_r.eval()
-    model_g.eval()
-    model_b.eval()
-    r_valid = 0
-    b_valid = 0
-    g_valid = 0
-    with torch.no_grad():
-        for img_name, lr_r_img, hr_r_img, lr_g_img, hr_g_img, lr_b_img, hr_b_img in v_loaded:
-            pred_r = model_r(lr_r_img)
-            pred_g = model_g(lr_g_img)
-            pred_b = model_b(lr_b_img)
-            
-            r_valid += torch.nn.functional.mse_loss(pred_r,hr_r_img, reduction="sum")
-            g_valid += torch.nn.functional.mse_loss(pred_g,hr_g_img, reduction="sum")
-            b_valid += torch.nn.functional.mse_loss(pred_b,hr_b_img, reduction="sum")
-    
-    r_valid = r_valid / len(v_loaded)
-    g_valid = g_valid / len(v_loaded)
-    b_valid = b_valid / len(v_loaded)
+    v_set = ImageDataset.ImageDataset(train_dataset, train_dir, target_dir, lr_h, lr_w, channel, FACTOR)
+    v_loaded = torch.utils.data.DataLoader(v_set, batch_size = 2*BATCH_SIZE)
+    for epoch in range(EPOCHS):
+        batch_num = 0
+        batch_losses = []
+        model_r.train()
+        # model_g.train()
+        # model_b.train()
+        for lr_r_img, hr_r_img in d_loaded:
+                print("EPOCH: " + str(epoch) + " BATCH: " + str(batch_num))
+                pred_r = model_r(lr_r_img)
+                # pred_g = model_g(lr_g_img)
+                # pred_b = model_b(lr_b_img)
+                
+                loss_r = torch.nn.functional.mse_loss(pred_r,hr_r_img, reduction="sum")
+                # loss_g = torch.nn.functional.mse_loss(pred_g,hr_g_img, reduction="sum")
+                # loss_b = torch.nn.functional.mse_loss(pred_b,hr_b_img, reduction="sum")
+                
+                batch_losses[0].append(float(loss_r))
+                # batch_losses[1].append(float(loss_g))
+                # batch_losses[2].append(float(loss_b))
+                
+                loss_r.backward()
+                optimizer_r.step()
+                optimizer_r.zero_grad()
+                # loss_g.backward()
+                # optimizer_g.step()
+                # optimizer_g.zero_grad()
+                # loss_b.backward()
+                # optimizer_b.step()
+                # optimizer_b.zero_grad()
+                batch_num+=1
+        
+        #Validation Loop After Each Epoch
+        model_r.eval()
+        # model_g.eval()
+        # model_b.eval()
+        r_valid = 0
+        # b_valid = 0
+        # g_valid = 0
+        with torch.no_grad():
+            for lr_r_img, hr_r_img in v_loaded:
+                pred_r = model_r(lr_r_img)
+                # pred_g = model_g(lr_g_img)
+                # pred_b = model_b(lr_b_img)
+                
+                r_valid += torch.nn.functional.mse_loss(pred_r,hr_r_img, reduction="sum")
+                # g_valid += torch.nn.functional.mse_loss(pred_g,hr_g_img, reduction="sum")
+                # b_valid += torch.nn.functional.mse_loss(pred_b,hr_b_img, reduction="sum")
+        
+        r_valid = r_valid / len(v_loaded)
+        # g_valid = g_valid / len(v_loaded)
+        # b_valid = b_valid / len(v_loaded)
 
-    print("RED Losses: " + str(batch_losses[0]))
-    print("BLUE Losses: " + str(batch_losses[1]))
-    print("GREEN Losses: " + str(batch_losses[2]))
-    print("EPOCH: " + str(epoch) + " Validation Loss (R,G,B): " + str(float(r_valid)), str(float(g_valid)), str(float(b_valid)))
-torch.save(model_r.state_dict(), 'model_r_weights.pth')
-torch.save(model_g.state_dict(), 'model_g_weights.pth')
-torch.save(model_b.state_dict(), 'model_b_weights.pth')
+        print("EPOCH: " + str(epoch) + " Validation Loss (R,G,B): " + str(float(r_valid)))#, str(float(g_valid)), str(float(b_valid)))
+        print("RED Batch Losses: " + str(batch_losses))
+    w_file = str("model_" + c[channel] + "weights.pth")
+    m_file = str("model_" + c[channel] + ".pth")
+    torch.save(model_r.state_dict(), w_file)
+    # torch.save(model_g.state_dict(), 'model_g_weights.pth')
+    # torch.save(model_b.state_dict(), 'model_b_weights.pth')
+    torch.save(model_r, m_file)
+    # torch.save(model_g, 'model_g.pth')
+    # torch.save(model_b, 'model_b.pth')
